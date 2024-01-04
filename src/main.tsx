dd47095554f145247912acf2a186732868ba9fbc
Odin Contreras <odin@foreach.cl>
2024-01-04 15:34:43 -0400

    add refetch config to tanstank query

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
